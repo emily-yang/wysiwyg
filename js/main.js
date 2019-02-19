187a3affa1689bf803179d1896e7b7dd69921d2d
emily-yang <cigoua@gmail.com>
2019-02-19 10:53:01 -0800

    toggle edit and save buttons Closes #4

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import toggleActive from './toggleActive.js';
 
 // Select edit buttons and add click event listener
-document.querySelectorAll('.edit-button')
+document.querySelectorAll('.edit-button, .save-button')
     .forEach(button => button.addEventListener('click', toggleActive));
+
