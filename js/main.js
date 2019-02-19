fa6007ef1c78405255a684dd61e4c4cfaefa0845
emily-yang <cigoua@gmail.com>
2019-02-19 09:54:19 -0800

    Adds modularity for editor toggle Resolves #5

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-// Select edit button
-const editButtons = document.querySelectorAll('.edit-button');
-// add event listener (click)
-editButtons.forEach(button => button.addEventListener('click', toggleActive));
-// function that adds a class to editable-text
-function toggleActive(event) {
-    // get the parent element
-    const editableText = event.target.parentElement;
-    // toggle the class active
-    editableText.classList.toggle('active');
-}
+import toggleActive from './toggleActive.js';
+
+// Select edit buttons and add click event listener
+document.querySelectorAll('.edit-button')
+    .forEach(button => button.addEventListener('click', toggleActive));
