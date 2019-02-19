2edc60489214d8ce0d5271b623651be018612a0a
emily-yang <cigoua@gmail.com>
2019-02-19 11:33:29 -0800

    Resolves #1

--- js/toggleActive.js
+++ js/toggleActive.js
@@ -6,6 +6,7 @@ export default function toggleActive(event) {
     // get the parent element
     const editableText = event.target.parentElement;
     // toggle the class active
+    console.log(event);
     editableText.classList.toggle('active');
 }
 
