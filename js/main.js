d73deed7d0f0e19d6b410ce09d2671f8e1ddd8db
emily-yang <cigoua@gmail.com>
2019-02-19 12:49:15 -0800

    Resolves #3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import toggleActive from './toggleActive.js';
 import handleEditorInput from './handleEditorInput.js';
+import addParagraph from './addParagraph.js';
 
 // Select edit buttons and add click event listener
 document.querySelectorAll('.edit-button, .save-button')
@@ -9,3 +10,4 @@ document.querySelectorAll('.edit-button, .save-button')
 // when fired, update text (or style)
 document.querySelectorAll('.editor').forEach(editor => editor.addEventListener('input', handleEditorInput));
 
+document.getElementById('add-button').addEventListener('click', addParagraph);
