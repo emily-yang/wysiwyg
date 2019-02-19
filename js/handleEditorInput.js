a8983baea0e86460f22ed8d5ce8ae6d4084cd918
emily-yang <cigoua@gmail.com>
2019-02-19 12:08:13 -0800

    add style changing Resolves #1

--- js/handleEditorInput.js
+++ js/handleEditorInput.js
@@ -1,7 +1,15 @@
 export default function handleEditorInput(event) {
+    const textElement = event.currentTarget.parentNode.querySelector('.text-element');
     if (event.target.type === 'text'|| event.target.tagName === 'TEXTAREA') {
-        const textElement = event.currentTarget.parentNode.querySelector('.text-element');
         const text = event.target.value;
         textElement.innerText = text;
+    } else {
+        // handle the styles
+        const { value, name: styleName } = event.target;
+        const unit = event.target.dataset.unit || '';
+        console.log(event.target.name);
+        console.log(unit);
+        textElement.style[styleName] = `${value}${unit}`;
+
     }
 }
